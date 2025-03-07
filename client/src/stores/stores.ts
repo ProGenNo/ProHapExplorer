ee2ab8622f6d6017432ceef69272199eafe6efd6
Jakub Vasicek <j.vasicek58@gmail.com>
2025-03-07 11:17:01 +0100

    change haplotype preprocessing + bug fixes

--- client/src/stores/stores.ts
+++ client/src/stores/stores.ts
@@ -1,5 +1,5 @@
 import { writable, derived } from "svelte/store";
-import type { Gene, Variant, Haplotype, Proteoform, Peptide } from '../types/graph_nodes'
+import type { Gene, Variant, Haplotype, Proteoform, Peptide, Transcript } from '../types/graph_nodes'
 import type { Writable } from 'svelte/store';
 
 export const geneSearchRequestPending: Writable<boolean> = writable(false)
@@ -20,7 +20,8 @@ export const highlightValues: Writable<string[]> = writable([])
 
 export const geneOverviewFiltered = derived([geneOverview, geneFilter], ([$geneOverview, $geneFilter]) => {
     const result =  $geneOverview.filter((g: Gene) => {
-        return ((($geneFilter[0][0] == -1) || (g._total_peptides! >= $geneFilter[0][0])) && 
+        return ((typeof(g._total_peptides) != "undefined") && (typeof(g._variant_peptides) != "undefined") && 
+                (($geneFilter[0][0] == -1) || (g._total_peptides! >= $geneFilter[0][0])) && 
                 (($geneFilter[1][0] == -1) || (g._total_peptides! <= $geneFilter[1][0])) && 
                 (($geneFilter[0][1] == -1) || (g._variant_peptides! >= $geneFilter[0][1])) && 
                 (($geneFilter[1][1] == -1) || (g._variant_peptides! <= $geneFilter[1][1])))
@@ -46,76 +47,104 @@ export const selectedVariant = derived([selectedGene, selectedVariantIdx], ([$se
     return $selectedGene.variants[$selectedVariantIdx];
 });
 
+const formatHaplotype = (haplotype: Haplotype, refProteoform: Proteoform, transcript: Transcript, haplo_idx: number) => {
+     // find the proteoform that is the result of this haplotype on the selected transcript
+     const proteoform = transcript.proteoforms.find(prot => prot.haplotype === haplotype)
+
+     if (!proteoform) {
+         return;
+     }
+
+     let finalHaplotype: Haplotype = {
+         id: haplotype.id,
+         included_variants: haplotype.included_variants,
+         matching_transcripts: haplotype.matching_transcripts,
+         matching_proteoform: proteoform,
+         frequency: transcript.transcript_hap_freqs[haplo_idx],
+         UTR5_protein: [],
+         UTR5_cDNA: [],
+         UTR3_protein: [],
+         UTR3_cDNA: [],
+         coding_protein: [],
+         coding_cDNA: [],
+         alt_found_flag: [],
+         synonymous_cDNA: []
+     }
+
+     // once we know the proteoform, we can filter out UTR and synonymous variants
+     const allProteinChanges = proteoform.protein_changes.split(';')
+     const allcDNAChanges = proteoform.cDNA_changes.split(';')
+     const hasStopCodon = proteoform.sequence.includes('*', proteoform.start_aa)
+
+     // consider the protein length either the length of the reference protein, or the length of the alternative protein if insertions are present
+     // do not hide variants in coding regions if an early stop codon is introduced
+     const protein_length = hasStopCodon ? 
+         Math.max(proteoform.sequence.indexOf('*', proteoform.start_aa) - proteoform.start_aa, refProteoform.sequence.length) : 
+         Math.max(proteoform.length - proteoform.start_aa, refProteoform.sequence.length)
+
+     for (let i=0; i < allProteinChanges.length; i++) {
+         const pos = Number.parseInt(allProteinChanges[i].split(':')[0])
+         if (pos < 0) {
+             // Is the variant in the 5'UTR?
+             finalHaplotype.UTR5_cDNA!.push('c.' + allcDNAChanges[i])
+             finalHaplotype.UTR5_protein!.push('p.' + allProteinChanges[i])
+         } else if (pos > protein_length) {
+             // Is the variant in the 3'UTR?
+             finalHaplotype.UTR3_cDNA!.push('c.' + allcDNAChanges[i])
+             finalHaplotype.UTR3_protein!.push('p.' + allProteinChanges[i])
+         } else {
+             // Is the variant non-synonymous?
+             const REF = allProteinChanges[i].split(':')[1].split('>')[0]
+             const ALT = allProteinChanges[i].split(':')[2].split('(')[0]
+             if ((REF !== ALT) || (allProteinChanges[i].includes('+fs'))) {
+                 finalHaplotype.coding_cDNA!.push('c.' + allcDNAChanges[i])
+                 finalHaplotype.coding_protein!.push('p.' + allProteinChanges[i])
+                 // Is there an overlapping variant peptide?
+                 finalHaplotype.alt_found_flag!.push(finalHaplotype.included_variants[i].found)
+             } else if (REF === ALT) {
+                 finalHaplotype.synonymous_cDNA!.push('c.' + allcDNAChanges[i])
+             }                    
+         }
+     }
+
+    return finalHaplotype
+}
+
 export const availableHaplotypes = derived([selectedTranscript, selectedVariant], ([$selectedTranscript, $selectedVariant]) => {
     if (!$selectedTranscript) return []
 
     const refProteoform = $selectedTranscript.canonical_protein
 
     let result: Haplotype[] = []
+
     $selectedTranscript.haplotypes.forEach((haplotype, idx) => {
         // if we have selected a variant, show only the haplotypes that contain this variant
         if (!$selectedVariant || haplotype.included_variants.map((variant) => variant.id).includes($selectedVariant.id)) {
-            // find the proteoform that is the result of this haplotype on the selected transcript
-            const proteoform = $selectedTranscript.proteoforms.find(prot => prot.haplotype === haplotype)
+            const finalHaplotype = formatHaplotype(haplotype, refProteoform, $selectedTranscript, idx)
 
-            if (!proteoform) {
-                return;
+            if (finalHaplotype) {
+                result.push(finalHaplotype)
             }
-
-            let finalHaplotype: Haplotype = {
-                id: haplotype.id,
-                included_variants: haplotype.included_variants,
-                matching_transcripts: haplotype.matching_transcripts,
-                matching_proteoform: proteoform,
-                frequency: $selectedTranscript.transcript_hap_freqs[idx],
-                UTR5_protein: [],
-                UTR5_cDNA: [],
-                UTR3_protein: [],
-                UTR3_cDNA: [],
-                coding_protein: [],
-                coding_cDNA: [],
-                alt_found_flag: [],
-                synonymous_cDNA: []
-            }
-
-            // once we know the proteoform, we can filter out UTR and synonymous variants
-            const allProteinChanges = proteoform.protein_changes.split(';')
-            const allcDNAChanges = proteoform.cDNA_changes.split(';')
-            const hasStopCodon = proteoform.sequence.includes('*', proteoform.start_aa)
-
-            // consider the protein length either the length of the reference protein, or the length of the alternative protein if insertions are present
-            // do not hide variants in coding regions if an early stop codon is introduced
-            const protein_length = hasStopCodon ? 
-                Math.max(proteoform.sequence.indexOf('*', proteoform.start_aa) - proteoform.start_aa, refProteoform.sequence.length) : 
-                Math.max(proteoform.length - proteoform.start_aa, refProteoform.sequence.length)
-
-            for (let i=0; i < allProteinChanges.length; i++) {
-                const pos = Number.parseInt(allProteinChanges[i].split(':')[0])
-                if (pos < 0) {
-                    // Is the variant in the 5'UTR?
-                    finalHaplotype.UTR5_cDNA!.push('c.' + allcDNAChanges[i])
-                    finalHaplotype.UTR5_protein!.push('p.' + allProteinChanges[i])
-                } else if (pos > protein_length) {
-                    // Is the variant in the 3'UTR?
-                    finalHaplotype.UTR3_cDNA!.push('c.' + allcDNAChanges[i])
-                    finalHaplotype.UTR3_protein!.push('p.' + allProteinChanges[i])
-                } else {
-                    // Is the variant non-synonymous?
-                    const REF = allProteinChanges[i].split(':')[1].split('>')[0]
-                    const ALT = allProteinChanges[i].split(':')[2].split('(')[0]
-                    if ((REF !== ALT) || (allProteinChanges[i].includes('+fs'))) {
-                        finalHaplotype.coding_cDNA!.push('c.' + allcDNAChanges[i])
-                        finalHaplotype.coding_protein!.push('p.' + allProteinChanges[i])
-                        // Is there an overlapping variant peptide?
-                        finalHaplotype.alt_found_flag!.push(finalHaplotype.included_variants[i].found)
-                    } else if (REF === ALT) {
-                        finalHaplotype.synonymous_cDNA!.push('c.' + allcDNAChanges[i])
-                    }                    
-                }
-            }
-
-            result.push(finalHaplotype)
         }
+    })
+
+    return result.sort((a, b) => (b.frequency! - a.frequency!))
+})
+
+export const allHaplotypes = derived([selectedGene], ([$selectedGene]) => {
+    if (!$selectedGene) return []
+
+    let result: Haplotype[] = []
+
+    $selectedGene.transcripts.forEach(transcript => {
+        transcript.haplotypes.forEach((haplotype, idx) => {
+            let finalHaplotype = formatHaplotype(haplotype, transcript.canonical_protein, transcript, idx)
+
+            if (finalHaplotype) {
+                finalHaplotype.matching_transcripts = [transcript]
+                result.push(finalHaplotype)
+            }
+        })
     })
 
     return result.sort((a, b) => (b.frequency! - a.frequency!))
